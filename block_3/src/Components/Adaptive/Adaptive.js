7132bae94a2a721c897b602444db6b21e3e36b45
v_seo@list.ru <your_github_email>
2022-12-09 01:25:35 +0300

    Adaprive examples

--- block_3/src/Components/Adaptive/Adaptive.js
+++ block_3/src/Components/Adaptive/Adaptive.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import pic from '../../img/p2.jpg'
 
 const Adaptive = () => {
     return (
-        <div>
-            Hello
+        <div className="news">
+            <div className="news__text">
+                <h2>Some header</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                    in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim
+                    id est laborum.</p>
+            </div>
+            <img src={pic} alt="some-image"/>
         </div>
+
     );
 };
 
